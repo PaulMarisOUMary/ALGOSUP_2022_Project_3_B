b0922ade1bd3387b66d9dd53ba0e8a1edaea535e
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2022-01-10 10:03:25 +0100

    add Visualization.Chart

--- Synth/Program.fs
+++ Synth/Program.fs
@@ -1,43 +1,36 @@
 namespace Synth
 
 open Synth
-
-module Graph =
-    open System
-    open XPlot.Plotly
-    open XPlot.GoogleCharts
-    
-    let a = 1.
-    let h = 0.
-    let b = 0.5
-    let k = 0.
-    let x = 0.
-    
-    let Sinus = [
-        for x in 0. .. 0.01 .. 20. do yield (x.ToString() , (a * Math.Sin((x  - h) / b) + k))
-        ]
-    let Square = [
-        for x in 0. .. 0.01 .. 20. do yield (x.ToString() , if (a * Math.Sin((x  - h) / b) + k) > 0. then 1. else -1.)
-        ]
-    let Triangle= [
-        for x in 0. .. 0.01 .. 20. do yield (x.ToString() , (Math.Abs(x%4.-2.)-1.))
-        ]
-    let Tooth = [
-        for x in 0. .. 0.01 .. 20. do yield (x.ToString() , ((x%2.)-1.))
-        ]
-    
-    let Waves = [ Sinus ; Square; Triangle; Tooth]
-    let Graph =
-        Waves
-        |> Chart.Combo
-        |> Chart.WithSize (1200, 250)
-    
-    Chart.Show Graph
+open System
 
 module Program =
     [<EntryPoint>]
     let main _  =
         printf "Hello world"
-        let test = Sample()
-        printfn $"test: {test.SampleMethod}"
+
+        let a = 1.
+        let h = 0.
+        let b = 0.5
+        let k = 0.
+        let x = 0.
+        
+        let Sinus = [
+            for x in 0. .. 0.01 .. 20. do yield (x , (a * Math.Sin((x  - h) / b) + k))
+            ]
+        let Square = [
+            for x in 0. .. 0.01 .. 20. do yield (x , if (a * Math.Sin((x  - h) / b) + k) > 0. then 1. else -1.)
+            ]
+        let Triangle= [
+            for x in 0. .. 0.01 .. 20. do yield (x , (Math.Abs(x%4.-2.)-1.))
+            ]
+        let Tooth = [
+            for x in 0. .. 0.01 .. 20. do yield (x , ((x%2.)-1.))
+            ]
+        
+        let Waves : List<List<float * float>> = [ Sinus ; Square; Triangle; Tooth]
+
+        Synth.Visualization.Chart(Waves)
+
+
+
         0
---
+++ Synth/Visualization/Chart.fs
@@ -0,0 +1,13 @@
+﻿namespace Synth
+
+open XPlot.Plotly
+open XPlot.GoogleCharts
+open System
+
+module Visualization =
+     let Chart datas =
+        let Graph =
+            datas 
+            |> Chart.Combo
+            |> Chart.WithSize (1200, 250)
+        Chart.Show Graph
