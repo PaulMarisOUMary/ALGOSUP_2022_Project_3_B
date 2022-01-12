2defa9ebdf7ba3520e4998c67e728b19170d5bad
MaximePagesAlgoSup <maxime.pages@algosup.com>
2022-01-12 15:04:06 +0100

    Added Note Type and octave frequency handler
    Co-Authored-By: arthur-lemo1ne <91249827+arthur-lemo1ne@users.noreply.github.com>

---
+++ Synth/Audio/Notes.fs
@@ -0,0 +1,34 @@
+﻿namespace Synth
+
+    module Note =
+
+        type Note =
+            | A
+            | B
+            | C
+            | D
+            | E
+            | F
+            | G
+
+        let getfrequency (note:Note) (octave:int) =
+            let frequencyBeforeOctave = 
+                match note with
+                | A -> 440.
+                | B -> 493.88
+                | C -> 261.63
+                | D -> 293.66
+                | E -> 329.63
+                | F -> 349.23
+                | G -> 392. in
+            match octave with
+                | 0 ->  frequencyBeforeOctave / 16.
+                | 1 ->  frequencyBeforeOctave / 8.
+                | 2 ->  frequencyBeforeOctave / 4.
+                | 3 ->  frequencyBeforeOctave / 2.
+                | 4 ->  frequencyBeforeOctave 
+                | 5 ->  frequencyBeforeOctave * 2.
+                | 6 ->  frequencyBeforeOctave * 4.
+                | 7 ->  frequencyBeforeOctave * 8.
+                | 8 ->  frequencyBeforeOctave * 16.
+                | _ -> raise (System.Exception($"Invalid Octave, expected 0< <8, got : {octave}"))
