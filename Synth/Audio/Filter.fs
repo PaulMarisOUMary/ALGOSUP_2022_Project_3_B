a866fbe12910aa9bb956c48e01b7aac79b1e1fe8
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2022-01-12 15:59:15 +0100

    convert Filter type -> module

--- Synth/Audio/Filter.fs
+++ Synth/Audio/Filter.fs
@@ -1,21 +1,21 @@
 ﻿namespace Synth
 
-type Filter =
-    member x.Amplitude = // Modify the wave’s amplitude by a fixed amount
+module Filter =
+    let Amplitude = // Modify the wave’s amplitude by a fixed amount
         0
 
-    static member Overdriven (amplitude : float) (wave : List<float * float>) =
+    let Overdriven (amplitude : float) (wave : List<float * float>) =
         [ for x, y in wave do
             if y < -amplitude then yield (x, -amplitude)
             elif y > amplitude then yield (x, amplitude)
             else yield (x, y)
         ]
 
-    member x.Echo = // Add echo to the sound
+    let Echo = // Add echo to the sound
         0
 
-    member x.Flange = // A flange effect filter, for a description of this effect see wikipedia: https://en.wikipedia.org/wiki/Flanging
+    let Flange = // A flange effect filter, for a description of this effect see wikipedia: https://en.wikipedia.org/wiki/Flanging
         0
 
-    member x.Reverb = // A reverb effect filter, wikipedia has a description of reverberation: https://en.wikipedia.org/wiki/Reverberation
+    let Reverb = // A reverb effect filter, wikipedia has a description of reverberation: https://en.wikipedia.org/wiki/Reverberation
         0
