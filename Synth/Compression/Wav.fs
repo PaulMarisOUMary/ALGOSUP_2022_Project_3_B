e3174018d0ebce63a8e2e68c161c40a7baa16976
MaximePagesAlgoSup <maxime.pages@algosup.com>
2022-01-14 14:04:47 +0100

    fixed Wav.fs Oooops forgor to commit yesterday

--- Synth/Compression/Wav.fs
+++ Synth/Compression/Wav.fs
@@ -10,13 +10,7 @@ module Wav =
     
   
 
-    let private EncoderWav (path:string)
-        (data:byte[])
-        (audioFormat:option<int16>)
-        (numChannels:option<int16>)
-        (sampleRate:option<int16>)
-        (blockAlign:option<int16>) 
-        (bitsPerSample:option<int16>) =
+    let EncodeWav (path:string) (data:byte[]) =
         
         let stream = File.Create(path)
         use writer = new BinaryWriter(stream)
@@ -27,26 +21,19 @@ module Wav =
 
         writer.Write("fmt "B) //fmt header
         writer.Write(16) //format size
-        if audioFormat.IsNone then writer.Write(1s) else writer.Write(audioFormat.Value) //Default value is 1 for uncompressed data
-        if numChannels.IsNone then writer.Write(1s) else writer.Write(numChannels.Value) //Default value is 1 for mono, 2 for stereo etc
-        if sampleRate.IsNone then writer.Write(8000) else writer.Write(int sampleRate.Value) //Default value is 8000 for average
-        
-        let (byteRate:int) = int ((if sampleRate.IsNone then  8000s else sampleRate.Value )
-            * (if numChannels.IsNone then 1s else numChannels.Value) 
-            * (if bitsPerSample.IsNone then 1s else bitsPerSample.Value))
-        writer.Write(byteRate) //ByteRate = sampleRate * numchannels * ByteperSample
+        writer.Write(1s) //Default value is 1 for uncompressed data
+        writer.Write(1s) //Default value is 1 for mono, 2 for stereo etc
+        writer.Write(Variables.sampleRate)         
+        let (byteRate:int) =  Variables.sampleRate * 16
+        writer.Write(byteRate) //ByteRate = sampleRate * ByteperSample
    
-        if blockAlign.IsNone then writer.Write(1s) else writer.Write(blockAlign.Value) //Default value is 1 for uncompressed data
-        if bitsPerSample.IsNone then writer.Write(8s) else writer.Write(bitsPerSample.Value) //Default value is 1 for uncompressed data
+        writer.Write(1s) //Default value is 1 for uncompressed data
+        writer.Write(16s) //Default value is 1 for uncompressed data
 
 
         writer.Write("data"B)
         writer.Write(data.Length)
         writer.Write(data)
-
-
-    let EncodetoWav path data parameters = 
-        if parameters |> List.length = 0 then EncoderWav path data None None None None None else EncoderWav path data None None None None None 
 
     let DecodeWav (path:string) = 
 
