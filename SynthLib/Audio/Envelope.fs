19b1f7eef0210d779f8ddc60a85e858ff3f8e7b5
arthur-lemo1ne <arthur.lemoine@algosup.com>
2022-01-24 14:14:59 +0100

    Update Envelope.fs
    Created the envelope effect

--- SynthLib/Audio/Envelope.fs
+++ SynthLib/Audio/Envelope.fs
@@ -1,8 +1,40 @@
-﻿namespace SynthLib.Audio
+﻿namespace SynthLib
 
 open System
 open SynthLib.Variables
 
 module Envelope =
-    let Sample =
-        0
+    let ADSR (wave : list<float>)=  //ADSR for Attack Decay Sustain and Release
+        [
+        //Parameter to modify the effect
+        let attackTime = 0.1
+        let attackAmplitude = 1.
+        let decayTime = 0.01
+        let releaseTime = 0.1
+        let sustainAmplitude = 0.8
+        let noteTime = (wave.Length/sampleRate)
+
+        let mutable output = 0.
+
+        let attackTimeSample = int(attackTime * (float wave.Length))
+        let decayTimeSample = int(decayTime * (float wave.Length))
+        let releaseTimeSample =  int(releaseTime * (float wave.Length))
+
+        for index in 0..wave.Length-1 do
+            //Attack time
+            if index < (int (attackTime * (float wave.Length))) then 
+                output <- ((float index)/(float attackTimeSample)) * wave[index]
+            //Decay time
+            elif index > attackTimeSample && index < (attackTimeSample+decayTimeSample) then
+                output <- ((float ((float index - float attackTimeSample)/float decayTimeSample)) * (sustainAmplitude-attackAmplitude) + attackAmplitude) * wave[index]
+            //Sustain time
+            elif index > (attackTimeSample+decayTimeSample) && index < (wave.Length-releaseTimeSample) then
+                output <- sustainAmplitude * wave[index]
+            //release time
+            elif index > (wave.Length-releaseTimeSample) then
+                output <- (((float wave.Length-1.)-(float index))/(float releaseTimeSample)) * (float wave[index]*sustainAmplitude)
+            
+            yield output
+
+        ]
+        
