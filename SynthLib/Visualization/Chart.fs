5ed8d4084696a3ffcb3076d0ed2fdfccf9977402
laurentbouquin <71769489+laurentbouquin@users.noreply.github.com>
2022-01-25 13:29:06 +0100

    Adding new file and functions
    Creating the Spectroscope file and function, and a new function for the chart visualisation.

--- SynthLib/Visualization/Chart.fs
+++ SynthLib/Visualization/Chart.fs
@@ -7,12 +7,21 @@ open System
 
 module Visualization =
 
-     let Chart datas xSize ySize =
+    let Chart datas xSize ySize =
      
         let Graph =
-            datas 
+            datas
             |> List.map ( fun l -> List.mapi (fun i x -> (float i / float sampleRate, x)) l)
             |> Chart.Combo
             |> Chart.WithSize (xSize, ySize)
         Chart.Show Graph
+
+    let ChartBar (datas: #value list list) (xSize: int) (ySize: int) =
+        let Graph =
+            datas
+            |> List.map ( fun l -> List.mapi (fun i x -> (float i / float sampleRate, x)) l)
+            |> XPlot.GoogleCharts.Chart.Bar
+            |> XPlot.GoogleCharts.Chart.WithSize (xSize, ySize)
+            |> XPlot.GoogleCharts.Chart.WithOptions (Options(orientation = "horizontal", colors=[|"#000000"|]))
+        XPlot.GoogleCharts.Chart.Show Graph
 
