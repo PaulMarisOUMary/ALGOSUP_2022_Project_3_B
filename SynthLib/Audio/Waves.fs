c940426857c7d46db9b3ce096146203bf8b5ea38
MaximePagesAlgoSup <maxime.pages@algosup.com>
2022-02-01 14:06:38 +0100

    Some Micro Optimizations on FIlters + added Identity Wave to reduce CPU Load on Blank Notes

--- SynthLib/Audio/Waves.fs
+++ SynthLib/Audio/Waves.fs
@@ -2,6 +2,7 @@
 
 open System
 open SynthLib.Variables
+open System.Collections.Generic
 
 module Wave =
 
@@ -9,7 +10,7 @@ module Wave =
 
     let (mod) x y=
         (x%y + y)%y
-      
+
     let Sine frequency t = sin(2. * PI * frequency * t)
 
     let Square frequency t = if (Math.Sin(2. * PI * frequency * t)) > 0. then 1. else - 1.
@@ -18,32 +19,41 @@ module Wave =
 
     let Sawtooth frequency t = ((2. * t * frequency) mod 2.) - 1.
 
+    let Identity frequency t = 0.
+
    // let Triangle frequency t = (8./(PI*PI))*(cos(2.0*PI*t*frequency)+ (1./9.)* cos(2.0*PI*t*frequency*3.)+ (1./25.)* cos(2.0*PI*t*frequency*5.))
 
 
     let MakeNote generator (duration:float) note octave =
         let calculatedFrequency = Note.GetFrequency note octave in
         [
-            for t in 0. .. 1. .. duration * (float sampleRate) do 
+            for t in 0. .. 1. .. duration * (float sampleRate) do
                 yield  t / float sampleRate |> generator calculatedFrequency
         ]
 
-    let Combine (args: List<List<float>>) =
-        (*let (sortedargs : List<List<float>>) = args |> List.sortBy(fun x -> x.Length)
+    let Combine (args: list<list<float>>) =
+(*
+        let sortedargs = args |> List.sortByDescending(fun x -> x.Length)
+        let mutable lastindex = (sortedargs |> List.tail |> List.head |> List.length ) - 1
+        let mutable out = new List<float>()
+//        for i in 0.. 1.. sortedargs|>
+ //           for
+
+        0
+        let (sortedargs : List<List<float>>) = args |> List.sortBy(fun x -> x.Length)
         let maxlen = sortedargs.[((sortedargs |> List.length ) - 2) ] |> List.length
         let mutable min = 0
         [
-            for x in 0.. 1.. maxlen - 1 do 
+            for x in 0.. 1.. maxlen - 1 do
                 if (x >= sortedargs[min].Length ) then min <- min + 1 else
-                yield  
+                yield
                     [
                         for y in min.. 1 .. (sortedargs.Length - 2)  do
                             yield sortedargs.[y].[x]
                     ] |> List.sum
         ]
+
         *)
-            
-                
         let maxlength = args |> List.map List.length |> List.max
         let refactorarraywith0 = [
             for x in args do
@@ -56,7 +66,7 @@ module Wave =
              for y in 0.. 1 ..maxlength-1 do
                 let listtosum = [for x in refactorarraywith0 do yield x.[y]]
                 listtosum |> List.map(fun x -> x/float listtosum.Length)|> List.sum
-                
+
         ]
        (* [
              for x in result do if x <> 0. then yield x / float args.Length
