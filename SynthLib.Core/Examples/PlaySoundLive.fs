c2befc1fcfcb268fd97c3d9b4496b1110c9b1c8c
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2022-02-08 14:51:40 +0100

    add few examples in .Core

---
+++ SynthLib.Core/Examples/PlaySoundLive.fs
@@ -0,0 +1,14 @@
+﻿namespace SynthLib.Core.Examples
+
+module PlaySoundLive =
+
+    open SynthLib.Audio
+
+    let Example =
+        // Create the sound
+        let ourWave = Wave.MakeNote Wave.Sine 1. Note.A 4
+
+        // Play the sound live in a loop
+        ourWave |>
+            Player.Play
+                true // true = loop
--- SynthLib.Core/Program.fs
+++ SynthLib.Core/Program.fs
@@ -10,25 +10,9 @@ module Program =
     [<EntryPoint>]
     let main _  =
 
-        // Visualize a Chart
-        //let Waves : List<List<float>> = [
-        //    Wave.MakeNote (Wave.Sawtooth) 1. Note.DEBUG 4
-        //]
-        //Synth.Visualization.Chart Waves 1200 250
-
-
-        // set Synth.Variables : samplerate env variable to true to save .Wav
+        // set Synth.Variables : samplerate env variable to true to save .wav
         let bitarray = MarioTheme |> Wav.ToByteArray
 
         Wav.EncodeWav @"../../../../test.wav" bitarray
-        
-
-        //let bitarray = Doom() |> List.map (fun (x:float) -> int16 (x * 32767.)) |> List.toArray
-
-        //use buffer = new SFML.Audio.SoundBuffer(bitarray,uint32 1,uint32 sampleRate)
-        //use sound = new SFML.Audio.Sound(buffer)
-        //sound.Loop <- true
-        //sound.Play()
-        //System.Console.Read()
 
         0
